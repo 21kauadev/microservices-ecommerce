ce6dede2bd7cc4ee8f23db5f0c141f58a45b2914
Kaua2123 <kauantnetsoares@gmail.com>
2025-10-06 09:13:47 -0300

    feat: Add all services in subfolders under app-root

---
+++ product-service/src/test/java/com/kaua_dev/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaua_dev.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
