55361d393b70fcc73c1e1c4b42c1644fcaff3e4a
Kaua2123 <kauantnetsoares@gmail.com>
2025-10-09 09:12:30 -0300

    feat: add User Entity model and migration of table user creation

---
+++ user-service/src/main/java/com/kaua_dev/user_service/domain/user/User.java
@@ -0,0 +1,50 @@
+package com.kaua_dev.user_service.domain.user;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 80, nullable = false)
+    private String name;
+
+    @Column(length = 100, unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+
+    public String getEmail() {return email;}
+    public void setEmail(String email) {this.email = email;}
+
+    public String getPassword() {return password;}
+    public void setPassword(String password) {this.password = password;}
+
+    public LocalDateTime getCreatedAt() {return createdAt;}
+
+    public LocalDateTime getUpdatedAt() {return updatedAt;}
+}
