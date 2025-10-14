aa49ebe9f4592d08c0ff1bf00c2ae89996f648d9
Kaua2123 <kauantnetsoares@gmail.com>
2025-10-14 10:23:11 -0300

    feat: add hash to user password in create and update routes

--- user-service/src/main/java/com/kaua_dev/user_service/service/UserService.java
+++ user-service/src/main/java/com/kaua_dev/user_service/service/UserService.java
@@ -4,7 +4,7 @@ import com.kaua_dev.user_service.domain.user.User;
 import com.kaua_dev.user_service.domain.user.UserDTO;
 import com.kaua_dev.user_service.domain.user.exceptions.UserNotFound;
 import com.kaua_dev.user_service.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,6 +18,10 @@ public class UserService {
         this.repository = repository;
     }
 
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     public List<User> findAll() {
         return repository.findAll();
     }
@@ -26,10 +30,12 @@ public class UserService {
     }
 
     public User create(UserDTO data) {
+        String hashPassword = passwordEncoder().encode(data.password());
+
         User user = new User();
         user.setName(data.name());
         user.setEmail(data.email());
-        user.setPassword(data.password());
+        user.setPassword(hashPassword);
 
         return repository.save(user);
     }
@@ -37,9 +43,13 @@ public class UserService {
     public User update(Long id, UserDTO data) {
         User user = repository.findById(id).orElseThrow(UserNotFound::new);
 
+
         if (data.name() != null) user.setName(data.name());
         if (data.email() != null) user.setEmail(data.email());
-        if (data.password() != null) user.setPassword(data.password());
+        if (data.password() != null) {
+            String hashPassword = passwordEncoder().encode(data.password());
+            user.setPassword(hashPassword);
+        }
 
         return repository.save(user);
     }
